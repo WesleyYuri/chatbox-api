69a500e6eb600b7203c8476ff57aa15ede3ade28
Wesley Yuri <crbwesley@gmail.com>
2023-04-05 13:59:15 +0000

    ChatBox-api

---
+++ src/main/java/br/com/wesley/chatboxapi/ChatboxApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.wesley.chatboxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatboxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatboxApiApplication.class, args);
+	}
+
+}
